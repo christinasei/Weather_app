fd6161fd6868e8c1e3f304e883c24cacedf9fc88
christinasei <104825885+christinasei@users.noreply.github.com>
2022-07-10 22:32:27 +0200

    created js-file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,44 @@
+function formatDate(timpestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = days[date.getDay()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+  ];
 
+  return `${day} ${hours}:${minutes}`;
+}
+function displayTemperature(response) {
+  console.log(response.data.wind.speed);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data.main.temp);
+  console.log(response.data.dt);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+let apiKey = "477f09ca1a2fb1bfcfeb8e5a31f57826";
+let city = "Vienna";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiURL).then(displayTemperature);
