ab02f82f8e99af653fc201c6d66263ff07c87b55
christinasei <104825885+christinasei@users.noreply.github.com>
2022-07-25 13:37:40 +0200

    change icon

--- src/app.js
+++ src/app.js
@@ -39,6 +39,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "477f09ca1a2fb1bfcfeb8e5a31f57826";
 let city = "Vienna";
